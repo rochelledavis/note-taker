0c9d278b8b4965fead383ec9359218fb49edf981
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-04-10 12:30:53 -0600

    htmlRoutes added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { notes } = require('../../db/db.json');
+
+//API GET request
+
+//API POST request
+
+//API DELETE request
+
+module.exports = router;
